a806b3e5124705b769f1e9a2c6d471c994ff9e9d
dcargs <cargilldevin@gmail.com>
2017-04-12 11:11:57 -0500

    modal for customer view added, functionality not working yet.
    For functionality I was thinking about printing out a table with just
    customer first and last name with 2 buttons. One for viewing specific
    details about the customer and another for editing the customer's info.
    
    They're both going to be bootstrap modals with AJAX calls to pull and
    edit the right information.

--- Website/main/views/customer.php
+++ Website/main/views/customer.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="../assets/css/stylesheet.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script src="../assets/javascript/scripts.js" charset="utf-8"></script>
     <title>Customer</title>
     <script>
@@ -64,7 +65,6 @@
             <div id="createCustomer">
               <form class="form-horizontal" action="customer.php" method="post">
                 <input type="hidden" name="formType" value="createCustomer">
-
                 <!-- user_id -->
                 <div class="row">
                   <div class="col-md-offset-2 col-md-3">
@@ -131,7 +131,78 @@
 
           <!-- edit customer -->
           <div class="col-md-*">
+            <button type="button" class="btn btn-info" data-toggle="modal" data-target="#myModal">Customer Information</button>
+            <!-- Modal -->
+            <div class="modal fade" id="myModal" role="dialog">
+              <div class="modal-dialog">
+                <!-- Modal content-->
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">Customer Information</h4>
+                  </div>
+                  <div class="modal-body">
+                    <div class="row">
+                      <div class="col-md-offset-2 col-md-3">
+                        <label for="usr">User:</label>
+                      </div>
+                      <div class="col-md-4">
+                        <input class="form-control" type="text" name="user_id" value="" readonly>
+                      </div>
+                    </div><hr>
+                    <!-- first_name -->
+                    <div class="row">
+                      <div class="col-md-offset-2 col-md-3">
+                        <label for="usr">First Name:</label>
+                      </div>
+                      <div class="col-md-4">
+                        <input class="form-control" type="text" name="first_name" value="" readonly>
+                      </div>
+                    </div><hr>
+                    <!-- last_name -->
+                    <div class="row">
+                      <div class="col-md-offset-2 col-md-3">
+                        <label for="usr">Last Name:</label>
+                      </div>
+                      <div class="col-md-4">
+                        <input class="form-control" type="text" name="last_name" value="" readonly>
+                      </div>
+                    </div><hr>
+                    <!-- email -->
+                    <div class="row">
+                      <div class="col-md-offset-2 col-md-3">
+                        <label for="usr">Email:</label>
+                      </div>
+                      <div class="col-md-4">
+                        <input class="form-control" type="text" name="email" value="" readonly>
+                      </div>
+                    </div><hr>
+                    <!-- phone_number -->
+                    <div class="row">
+                      <div class="col-md-offset-2 col-md-3">
+                        <label for="usr">Phone Number:</label>
+                      </div>
+                      <div class="col-md-4">
+                        <input class="form-control" type="text" name="phone_number" value="" readonly>
+                      </div>
+                    </div><hr>
+                    <!-- address -->
+                    <div class="row">
+                      <div class="col-md-offset-2 col-md-3">
+                        <label for="usr">Address:</label>
+                      </div>
+                      <div class="col-md-4">
+                        <input class="form-control" type="text" name="address" value="" readonly>
+                      </div>
+                    </div>
 
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
 
           <!-- select customer aka view info -->
